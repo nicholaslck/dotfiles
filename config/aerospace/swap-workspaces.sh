a43b790d887e024115564e02487f7522a893977c
Nicholas Lau <nicholaslck@gmail.com>
2026-02-17 20:58:34 +1300

    fix(sketchybar): add/remove app window did not update the items

--- config/aerospace/swap-workspaces.sh
+++ config/aerospace/swap-workspaces.sh
@@ -3,16 +3,16 @@
 TARGET_WS=$1
 
 if [ -z "$TARGET_WS" ]; then
-	echo "Usage: $0 <target_workspace>"
-	exit 1
+  echo "Usage: $0 <target_workspace>"
+  exit 1
 fi
 
 # 1. Get the current workspace
 CURRENT_WS=$(aerospace list-workspaces --focused)
 
 if [ "$CURRENT_WS" == "$TARGET_WS" ]; then
-	echo "Cannot swap with self."
-	exit 0
+  echo "Cannot swap with self."
+  exit 0
 fi
 
 # 2. get all windows from current workspace and target workspace
@@ -22,10 +22,10 @@ LIST_TARGET=$(aerospace list-windows --workspace "$TARGET_WS" --format "%{window
 # 3. exchange window
 # move all target windows to current
 for wid in $LIST_TARGET; do
-	aerospace move-node-to-workspace --window-id "$wid" "$CURRENT_WS"
+  aerospace move-node-to-workspace --window-id "$wid" "$CURRENT_WS"
 done
 
 # move all current windows to target
 for wid in $LIST_CURRENT; do
-	aerospace move-node-to-workspace --window-id "$wid" "$TARGET_WS"
+  aerospace move-node-to-workspace --window-id "$wid" "$TARGET_WS"
 done
--- config/sketchybar/plugins/aerospace.sh
+++ config/sketchybar/plugins/aerospace.sh
@@ -2,57 +2,90 @@
 
 source $CONFIG_DIR/constants.sh
 
-WORKSPACES=("1" "2" "3" "4" "5" "6" "7" "8" "9")
+workspace_apps=()
 
-if [ -z "$FOCUSED_WORKSPACE" ]; then
+get_workspace_apps() {
+  while IFS='-' read -r window_id workspace app; do
+    if [[ -n "${workspace_apps[$workspace]}" ]]; then
+      workspace_apps[$workspace]+="/$app"
+    else
+      workspace_apps[$workspace]="$app"
+    fi
+  done < <(aerospace list-windows --all --format "%{window-id}-%{workspace}-%{app-name}" | sort -t'-' -k1,1n)
+}
+
+args=()
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  for sid in "${WORKSPACES[@]}"; do
+    if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
+      background_drawing=on
+      icon_color=$COLOR_FOREGROUND_PRIMARY
+      label_color=$COLOR_FOREGROUND_PRIMARY
+    elif [ "$sid" = "$PREV_WORKSPACE" ]; then
+      background_drawing=off
+      icon_color=$COLOR_FOREGROUND_PRIMARY
+      label_color=$COLOR_FOREGROUND_PRIMARY
+    else
+      background_drawing=off
+      icon_color=$COLOR_FOREGROUND_SECONDARY
+      label_color=$COLOR_FOREGROUND_SECONDARY
+    fi
+    args+=(
+      --set "space.$sid"
+      background.drawing=$background_drawing
+      icon.color=$icon_color
+      label.color=$label_color
+    )
+  done
+  sketchybar "${args[@]}"
+
+elif [ "$SENDER" = "space_windows_change" ]; then
+  # new window added or deleted
+  get_workspace_apps
+
+  for sid in "${WORKSPACES[@]}"; do
+    app_names="${workspace_apps[$sid]:-}"
+    label="$app_names"
+    args+=(
+      --set "space.$sid"
+      label="$label"
+    )
+  done
+  sketchybar "${args[@]}"
+else
+  # initialization
   FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused)
+  get_workspace_apps
+
+  for sid in "${WORKSPACES[@]}"; do
+
+    app_names="${workspace_apps[$sid]:-}"
+    icon="$sid:"
+    label="$app_names"
+
+    background_color=$COLOR_BACKGROUND_ACTIVE
+    background_drawing=off
+    icon_color=$COLOR_FOREGROUND_SECONDARY
+    label_color=$COLOR_FOREGROUND_SECONDARY
+
+    if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
+      background_drawing=on
+      icon_color=$COLOR_FOREGROUND_PRIMARY
+      label_color=$COLOR_FOREGROUND_PRIMARY
+    fi
+
+    args+=(
+      --set "space.$sid"
+      background.color=$background_color
+      background.drawing=$background_drawing
+      icon.color=$icon_color
+      label.color=$label_color
+      icon="$icon"
+      label="$label"
+    )
+  done
 fi
 
-##### Get all active workspaces #####
-# active_workspaces=$(aerospace list-workspaces --all)
-
-##### Get all app names #####
-workspace_apps=()
-while IFS='-' read -r workspace app; do
-  if [[ -n "${workspace_apps[$workspace]}" ]]; then
-    workspace_apps[$workspace]+=" / $app"
-  else
-    workspace_apps[$workspace]="$app"
-  fi
-done < <(aerospace list-windows --all --format "%{workspace}-%{app-name}")
-
-##### Re-draw each items #####
-args=()
-for sid in "${WORKSPACES[@]}"; do
-
-  app_names=${workspace_apps[$sid]}
-  background_color=$COLOR_BACKGROUND_ACTIVE
-
-  background_drawing=off
-  icon_color=$COLOR_FOREGROUND_SECONDARY
-  label_color=$COLOR_FOREGROUND_SECONDARY
-
-  icon="$sid:"
-  label="$app_names"
-
-  if [ "$sid" = "$FOCUSED_WORKSPACE" ]; then
-    background_drawing=on
-    icon_color=$COLOR_FOREGROUND_PRIMARY
-    label_color=$COLOR_FOREGROUND_PRIMARY
-  elif [ "$sid" = "$PREV_WORKSPACE" ]; then
-    icon_color=$COLOR_FOREGROUND_PRIMARY
-    label_color=$COLOR_FOREGROUND_PRIMARY
-  fi
-
-  args+=(
-    --set "space.$sid"
-    background.color=$background_color
-    background.drawing=$background_drawing
-    icon.color=$icon_color
-    label.color=$label_color
-    icon="$icon"
-    label="$label"
-  )
-done
-
-sketchybar "${args[@]}"
+if [ ${#args[@]} -gt 0 ]; then
+  sketchybar "${args[@]}"
+fi
